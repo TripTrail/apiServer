b72de6743ecd661873af02bbb55a68b5fc2cab53
Ashutosh Meher <ashutosh.meher@tothenew.com>
2018-12-05 15:00:04 +0530

    post

--- src/main/java/com/company/application/controller/PublicController.java
+++ src/main/java/com/company/application/controller/PublicController.java
@@ -10,9 +10,9 @@ import com.company.application.service.RegistrationService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +22,7 @@ public class PublicController {
     @Autowired
     private RegistrationService registrationService;
 
-    @RequestMapping(value = API_PUBLIC_USER_REGISTRATION_URL, method = RequestMethod.POST)
+    @PostMapping(value = API_PUBLIC_USER_REGISTRATION_URL)
     public APIResponse registerNewUser(@RequestBody @Valid RegistrationDetailsCO registrationDetailsCO,
                                        BindingResult result) throws ValidationException {
 
